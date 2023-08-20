283b27f0219ea9f7b7172abf7e640c1194be418d
sirpolux <poluxreloaded@gmail.com>
2023-08-20 15:39:03 +0100

    feat-added Security filter chain

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfiguration.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.luv2code.springboot.cruddemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.User;
 
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfiguration {
@@ -26,5 +30,25 @@ public class DemoSecurityConfiguration {
                 .roles("EMPLOYEE","MANAGER","ADMIN").build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees").hasRole("ADMIN")
+                );
+        //USE Http Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        //disable cross site request forgery
+        //in general, not required for stateless REST APis that use POST, PUT, DELETE ad/or PATCH
+
+        http.csrf(csrf->csrf.disable());
+
+        return null;
     }
 }
